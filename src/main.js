619d52ab9fd507fdde9c640e3a53a007ee9545b4
弄月 <1090195622@qq.com>
2020-04-26 10:01:27 +0800

    同步

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
-import Vue from 'vue';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 
-import App from './App.vue';
-import router from './router';
-import store from './store';
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+import moment from "moment";
+import "moment/locale/zh-cn";
+
+console.log(moment().format("dddd"));
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  mounted() {
+    // document.dispatchEvent(new Event("render-event"));
+  }
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,15 @@
 <template>
-  <div class="home">
-    <!-- template中使用环境变量 -->
-    API: {{api}}
-    <br>
-    {{name}}
-    <img alt="Vue logo"
-         src="@/assets/logo.png">
-    <svg-icon icon-class="password"></svg-icon>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-import axios from 'axios'
-
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      api: process.env.VUE_APP_API,
-      name: ''
-    }
-  },
+  name: "home",
   mounted() {
-    // js代码中使用环境变量
-    console.log("BASE_URL: ", process.env.BASE_URL);
-    console.log("VUE_APP_API: ", process.env.VUE_APP_API);
-
-    axios.get('/api/1').then(res => {
-      console.log('proxy:', res);
-    })
-
+    axios.get("/api/1").then(res => {
+      console.log(res);
+    });
   }
-}
+};
 </script>
-
-
-<style>
-.nihao {
-  background: blue;
-}
-</style>
